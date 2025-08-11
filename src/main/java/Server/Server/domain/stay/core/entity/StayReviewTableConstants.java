73cfb8a0f3bf383d5d827543b48eb630bb87747a
seahee <lollol0617@gmail.com>
2025-08-11 15:53:46 +0900

    [feat] #1 - stayReview entity, constants, vo 작성

---
+++ src/main/java/Server/Server/domain/stay/core/entity/StayReviewTableConstants.java
@@ -0,0 +1,18 @@
+package Server.Server.domain.stay.core.entity;
+
+public final class StayReviewTableConstants {
+
+	private StayReviewTableConstants() {
+	}
+
+	public static final String TABLE_STAY_REVIEW = "stay_review";
+
+	public static final String COLUMN_ID = "id";
+	public static final String COLUMN_REVIEWER_ID = "reviewer_id";
+	public static final String COLUMN_STAY_ID = "stay_id";
+	public static final String COLUMN_CREATED_AT = "created_at";
+	public static final String COLUMN_RATE = "rate";
+	public static final String COLUMN_CONTENT = "content";
+	public static final String COLUMN_HOST_COMMENT = "host_comment";
+	public static final String COLUMN_HOST_COMMENT_CREATED_AT = "host_comment_created_at";
+}
