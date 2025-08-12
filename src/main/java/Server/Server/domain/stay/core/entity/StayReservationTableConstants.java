35a9ced82ca01324e5f2fbf1e35b56a0f6cfe660
seahee <lollol0617@gmail.com>
2025-08-12 19:53:26 +0900

    [feat] #1 - StayReservation entity, constants, vo 작성

---
+++ src/main/java/Server/Server/domain/stay/core/entity/StayReservationTableConstants.java
@@ -0,0 +1,16 @@
+package Server.Server.domain.stay.core.entity;
+
+public final class StayReservationTableConstants {
+
+	private StayReservationTableConstants() {}
+
+	public static final String TABLE_STAY_RESERVATION = "stay_reservation";
+
+	public static final String COLUMN_ID = "id";
+	public static final String COLUMN_GUEST_ID = "guest_id";
+	public static final String COLUMN_ROOM_ID = "room_id";
+	public static final String COLUMN_CHECK_IN = "check_in";
+	public static final String COLUMN_CHECK_OUT = "check_out";
+	public static final String COLUMN_GUEST_COUNT = "guest_count";
+	public static final String COLUMN_CREATED_AT = "created_at";
+}
